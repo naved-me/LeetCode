fde9f102cc8c2a11f587d325e832450250ce7ecc
Syed Naved M <syednavedmehdi@gmail.com>
2026-08-05 18:48:06 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.14%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        count = defaultdict(int)
+        for i in range (len(nums)):
+            j = nums[i]
+            if j == 0:
+                count[0] += 1
+            elif j == 1 :
+                count[1] += 1
+            elif j == 2 :
+                count[2] += 1
+        nums[:] = [0] * count[0] + [1] * count[1] + [2] * count[2] 
+        return nums

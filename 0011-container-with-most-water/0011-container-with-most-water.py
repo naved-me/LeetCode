45ff96a0f0399d5801ffca509aea31ccaf0ba631
Syed Naved M <syednavedmehdi@gmail.com>
2026-08-13 16:12:44 +0530

    Time: 59 ms (52.11%), Space: 29.6 MB (37.53%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,16 +3,13 @@ class Solution:
         left = 0
         right = len(height) - 1
         max_area = 0
-        
         while left < right:
             # Calculate current area
             current_area = min(height[left], height[right]) * (right - left)
             max_area = max(max_area, current_area)
-            
             # Move the pointer pointing to the shorter line
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-                
         return max_area
